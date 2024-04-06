5e6d11d5b292fedd4d6b0d153f7ae06144017316
railme37509124 <166141133+railme37509124@users.noreply.github.com>
2024-04-06 13:50:58 +0100

    Update 6872274481.lua

--- 6872274481.lua
+++ 6872274481.lua
@@ -10071,7 +10071,7 @@ end)
                         repeat task.wait()
                             game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("node_modules"):WaitForChild("@rbxts"):WaitForChild("net"):WaitForChild("out"):WaitForChild("_NetManaged"):WaitForChild("RocketImpulse"):InvokeServer(unpack({
                                 [1] = {
-                                    ["velocity"] = Vector3.new(9e9, 9e9, 9e9)
+                                    ["velocity"] = Vector3.new(math.huge, math.huge, math.huge)
                                 }
                             }))
                         until (not AnticheatDisabler.Enabled)
